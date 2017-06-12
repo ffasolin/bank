b0899e6a41017cbdab4339254b3ba15df6a6ca74
Filipe Fasolin <ffasolin@gmail.com>
2017-06-12 11:33:50 +0100

    Adds beforeEach hook to AccountSpec

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,13 +1,15 @@
 describe('Account', function() {
   var account;
 
-  it('is defined', function() {
+  beforeEach(function() {
     account = new Account;
+  });
+
+  it('is defined', function() {
     expect(account).toBeDefined();
   });
 
   it('has an initial balance of 0', function() {
-    account = new Account;
     expect(account._balance).toBe(0);
   });
 });
