a98c52cdbb9dae8522befff23ce4bff962648b36
Filipe Fasolin <ffasolin@gmail.com>
2017-06-12 17:53:24 +0100

    Prints full statement

--- spec/StatementPrinterSpec.js
+++ spec/StatementPrinterSpec.js
@@ -1,7 +1,9 @@
 const StatementPrinter = require('../src/StatementPrinter.js');
+const Atm = require('../src/Atm.js');
 
 describe('StatementPrinter', function() {
   var receipt;
+  var atm;
 
   it('is defined', function() {
     expect(StatementPrinter).toBeDefined();
@@ -10,5 +12,18 @@ describe('StatementPrinter', function() {
   it('returns a header', function() {
     receipt = new StatementPrinter;
     expect(receipt.header()).toBe('date || credit || debit || balance');
+  });
+
+  it('prints full current statement', function() {
+    receipt = new StatementPrinter;
+    atm = new Atm();
+    atm._makeDeposit(1000);
+    atm._makeDeposit(2000);
+    atm._makeWithdrawal(500);
+    console.log = jasmine.createSpy("log");
+    receipt.fullStatement(atm._deposits, atm._withdrawals);
+    expect(console.log).toHaveBeenCalledWith('12/06/2017 || || 1000 || 1000');
+    expect(console.log).toHaveBeenCalledWith('12/06/2017 || || 2000 || 3000');
+    expect(console.log).toHaveBeenCalledWith('12/06/2017 || 500 || || 2500');
   });
 });
--- src/StatementPrinter.js
+++ src/StatementPrinter.js
@@ -4,4 +4,21 @@ StatementPrinter.prototype.header = function() {
   return 'date || credit || debit || balance';
 };
 
+StatementPrinter.prototype.fullStatement = function(deposits, withdrawals) {
+  this.depositsStatement(deposits);
+  this.withdrawalsStatement(withdrawals);
+};
+
+StatementPrinter.prototype.depositsStatement = function(deposits) {
+  for (var i = 0; i < deposits.length; i++) {
+    console.log(`${deposits[i][0]} || || ${deposits[i][1]} || ${deposits[i][2]}`)
+  }
+};
+
+StatementPrinter.prototype.withdrawalsStatement = function(withdrawals) {
+  for (var i = 0; i < withdrawals.length; i++) {
+    console.log(`${withdrawals[i][0]} || ${withdrawals[i][1]} || || ${withdrawals[i][2]}`)
+  }
+};
+
 module.exports = StatementPrinter;
